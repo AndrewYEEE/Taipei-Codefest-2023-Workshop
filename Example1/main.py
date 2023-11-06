71a3823db0d2b20ae7aec05b40ef8715acdf4a8c
AndrewChao <andrew33673367@cht.com.tw>
2023-11-06 17:31:16 +0800

    11/06 筆記

---
+++ Example1/main.py
@@ -0,0 +1,152 @@
+
+import geopandas as gpd
+import geocoder
+import json
+import pandas as pd
+from shapely.geometry import LineString, Point, Polygon
+import func
+
+
+## **A.臺北市身心障礙者福利服務 -> 無障礙需求歷年趨勢**
+
+### Designing Processing Plan
+"""
+(實際步驟是先了解`Input data`與`Output data`才制定出`處理計畫`，但為了閱讀方便，將計畫放在最前面)
+1. 修改欄位名
+2. 民國年修改成西元年
+3. 定義data type
+4. 選擇指定欄位
+"""
+
+print("目標: A.臺北市身心障礙者福利服務 -> 無障礙需求歷年趨勢")
+
+# Want Ouput data: 無障礙需求歷年趨勢
+print("Want Ouput data: 無障礙需求歷年趨勢")
+FILE_NAME = ".\Datasets\Processed\無障礙需求歷年趨勢.csv"
+accessibility_need = pd.read_csv(FILE_NAME)
+print(accessibility_need.head())
+
+
+# Input data- 臺北市身心障礙者福利服務
+print("Input data- 臺北市身心障礙者福利服務")
+disability_welfare = func.get_datataipei_api('a09d5ff1-6b83-4bce-abbb-480b126db611')
+print(disability_welfare.head(3))
+print(disability_welfare.dtypes)
+
+
+
+
+"""
+### ETL code
+
+預設儲存資料於`/content`路徑下
+"""
+
+import pandas as pd
+# Designing Processing Plan
+# 1. 修改欄位名，[]變成()
+# 2. 民國年修改成西元年
+# 3. 定義data type，具量詞欄位應為float
+
+# 0.Set Config
+RID = 'a09d5ff1-6b83-4bce-abbb-480b126db611'
+# GITHUB_DATA_URL = 'https://raw.githubusercontent.com/tpe-doit/Taipei-Codefest-2023-Workshop/2-Initial-Data-Cleaning-Visualization/Datasets/Processed/%E7%84%A1%E9%9A%9C%E7%A4%99%E9%9C%80%E6%B1%82%E6%AD%B7%E5%B9%B4%E8%B6%A8%E5%8B%A2.csv?token=GHSAT0AAAAAACHYWWNICD23CYPEE25UQQSSZJXOERA'
+GITHUB_DOWNLOAD_FILE_NAME = ".\Datasets\Processed\無障礙需求歷年趨勢.csv"
+
+
+# 1.Collection
+# input 臺北市身心障礙者福利服務
+print("input 臺北市身心障礙者福利服務")
+raw_disability_welfare = func.get_datataipei_api(RID)
+disability_welfare = raw_disability_welfare.copy() 
+
+
+# 2.Inspection
+print('臺北市身心障礙者福利服務:')
+print(disability_welfare.columns)
+
+print('先檢查一遍有無NaN資料: ')
+print(disability_welfare.isna().sum(axis=0)) #axis=0代表以Column做為Sum的標的
+
+print('實際以"住宿及照顧福利機構核定服務人數"檢查，發現空資料是以"-"表示而不是NaN:')
+print(disability_welfare['住宿及照顧福利機構核定服務人數[人]'].value_counts(dropna=False)) #dropna=False代表不丟棄NaN
+
+# 3.Rename
+# 修改欄位名，[]變成()
+# disability_welfare.rename(columns={'old_column_name': 'new_column_name'}, inplace=True)
+# disability_welfare.columns = ['column_name1', 'column_name2']
+
+print('Rename 將所有Column中"[]"換成"()"、替換"年份":')
+col_map = {}
+for col in disability_welfare.columns:
+    new_col = col.replace('[', '(').replace(']', ')')
+    col_map[col] = new_col
+disability_welfare.rename(columns=col_map, inplace=True) #inplace: 是否要改變原數據，False是不改變，True是改變，預設是False
+disability_welfare.rename(columns={'年別': '年份'}, inplace=True)
+print(disability_welfare.columns)
+
+# 4.Define data type
+# 定義data type，具量詞欄位應為float
+# (int無法容許np.nan!!!!!)
+print("Define data type (int無法容許np.nan)")
+
+# data.Chinese === data["Chinese"]
+disability_welfare['年份'] = disability_welfare['年份'].astype(str) #將指定的Column資料轉換成指定類型
+df_row_len, df_col_len = disability_welfare.shape #獲取row,col數量
+for col_index in range(3, df_col_len): #從第3的col往後獲取
+    col_name = disability_welfare.columns[col_index] #獲取該col的名稱
+    fine_col = disability_welfare[col_name].copy() #將該col擁有的rows複製
+    # panda可以直接對整個rows進行邏輯運算
+    # 運算結果會以bool array[]方式回傳
+    is_dash = (fine_col=='-') #全部檢查滿足=='-'者標記為True
+    fine_col.loc[is_dash] = None #將標記為True的row設為None先清空
+    fine_col = fine_col.astype(float)  #當清空的欄位轉成float時，就會是NaN
+    disability_welfare[col_name] = fine_col #整個rows複寫回去
+print(disability_welfare)
+
+
+
+print('重新檢查一遍有無NaN資料: ')
+print(disability_welfare.isna().sum(axis=0)) #axis=0代表以Column做為Sum的標的
+
+# DataFrame.iloc[“索引數字”, “欄位數字”]
+# data.iloc[:,0]
+# data.iloc[:,:2]
+# describe: 一次求得計數(count), 均值(mean), 標準差(std), max, min, 中位數, 1/4位數, 3/4位數
+print(disability_welfare.iloc[:, 3:].describe())
+
+# 5.Fill missing value
+# 這部份範例沒有實作，單純看我們自己要怎麼處裡
+print("Fill missing value (這部份範例沒有實作，單純看我們自己要怎麼處裡)")
+
+# 6. ......other process......
+print("其他處理")
+# 民國年修改成西元年
+# panda中內建的字串處裡功能都在.str中，例如: .str.cat()、.str.replace()、.str.split()....
+chinese_year = disability_welfare['年份'].str.replace('年', '')  #取代rows內字串
+bc_year = chinese_year.astype(int) + 1911 #民國 轉 西元
+bc_year = bc_year.astype(str) + '年' # 補回 "年"
+disability_welfare['年份'] = bc_year # 塞回"年分" column
+print(disability_welfare['年份'])
+
+# 7.Select
+# 年份                 object
+# 日間及住宿式照顧補助金額(元)    object
+# 生活補助金額(元)          object
+# 輔具補助金額(元)          object
+print("資料篩選")
+select_col = ['年份', '日間及住宿式照顧補助金額(元)', '生活補助金額(元)', '輔具補助金額(元)']
+disability_welfare = disability_welfare[select_col] #獲取指定欄位
+disability_welfare.head()
+
+# 8.Save
+OUTPUT_FILENAME = 'disability_welfare_over_the_years.csv'
+disability_welfare.to_csv(OUTPUT_FILENAME, index=False, encoding='UTF-8')
+
+print("本範例缺點: ")
+print("1. 並沒有排除NaN欄位或初始化NaN欄位")
+print("2. 中途由於NaN，將欄位值皆轉float，但實際上應該要先檢查欄位是否適合轉float")
+print("3. 最後輸出仍保持float，但金額應該要以int比較適合，但int不支援NaN")
+
+
+
---
+++ Example2/main.py
@@ -0,0 +1,283 @@
+
+import geopandas as gpd
+import geocoder
+import json
+import pandas as pd
+from shapely.geometry import LineString, Point, Polygon
+import func
+
+
+
+## **B.臺北市身障設施+臺北市身障社區長照機構+村里界 -> 身障友善機構**
+
+# Output data- 身障友善機構
+print("預想的結果:")
+FILE_NAME = '.\Datasets\Processed\身障友善機構.csv'
+friendly_institution = pd.read_csv(FILE_NAME)
+
+print(friendly_institution.head())
+
+# Input 1. 身障設施
+print("Input 1. 臺北市身障設施 (身障機構收容暨空位狀態)")
+# URL = 'https://data.taipei/api/frontstage/tpeod/dataset/resource.download?rid=57d8db7f-21a9-413f-8158-1df3b3d66d75'
+# FILE_NAME = 'disable_institutions.zip'
+# !wget $URL -O $FILE_NAME
+# !unzip -d disable_institutions -O BIG5 $FILE_NAME
+
+# shp是一種儲存地理圖資的檔案格式，一定至少包含 3 個檔案： .shp (記錄地理圖資的點線面資訊)， .shx (地理圖資的索引)，.dbf (地理圖資的屬性資料)；
+disable_institution = gpd.read_file('.\Datasets\Raw\身障設施_202309\身障設施_202309.shp')
+print(disable_institution.head(3))
+
+# Input 2. 臺北市身障社區長照機構
+print("Input 2. 臺北市身障社區長照機構")
+rid = '5e6dd32d-ac91-48cd-87ab-776fcc4811b7'
+raw_disability_community_longterm_care_institutions = func.get_datataipei_api(rid)
+dis_longterm = raw_disability_community_longterm_care_institutions.copy()
+print(dis_longterm)
+
+
+
+"""
+### ETL Code
+將`身障機構收容暨空位狀態`與`臺北市身障社區長照機構`合併成一個資料集，並產生適合的空間資訊。
+1. 一致的columns name
+2. 正確的column type
+3. `臺北市身障社區長照機構`須加上"屬性"
+4. 利用地址為`臺北市身障社區長照機構`加上行政區
+5. (下半)利用Geocoder取得座標
+6. (下半)產生TWD97座標值
+7. (下半)利用村里界取得行政區
+8. (下半)合併`身障機構收容暨空位狀態`與`臺北市身障社區長照機構`
+"""
+
+
+# 0.Set Config
+LONGTERM_RID = '5e6dd32d-ac91-48cd-87ab-776fcc4811b7'
+GITHUB_DATA_URL = 'https://raw.githubusercontent.com/tpe-doit/Taipei-Codefest-2023-Workshop/3-ETL/Datasets/Processed/%E8%BA%AB%E9%9A%9C%E5%8F%8B%E5%96%84%E6%A9%9F%E6%A7%8B.csv?token=GHSAT0AAAAAACHYWWNIZWPL36D3VIXR74BUZJY233Q'
+GITHUB_DOWNLOAD_FILE_NAME = 'disable_friendly_institution.csv'
+OUTPUT_FILENAME = 'disable_friendly_facility.csv'
+
+
+
+# 1.Collection
+# Input- 身障機構收容暨空位狀態
+# 臺北市身障設施
+print("Input 1. 重新讀取 臺北市身障設施 (以EPSG:3826格式轉碼座標)")
+raw_disable_institution = gpd.read_file(
+    f'.\Datasets\Raw\身障設施_202309\身障設施_202309.shp', from_crs='EPSG:3826'
+)
+dis_institution = raw_disable_institution.copy()
+
+# 1.5 remove the institutions in New Taipei City
+# .str是獲取panda內建的string API
+# .str.startswith() 判斷個row data是否以给定的字符串开头
+# 位元運算子〜是補碼運算子。如果操作數為1，則傳回0，如果為0，則傳回1
+# 篩選Pandas DataFrame資料: df[df["math"] > 80]
+print( ~dis_institution.address.str.startswith('新北市'))
+dis_institution = dis_institution[
+    ~dis_institution.address.str.startswith('新北市')]
+
+print(dis_institution.head(3))
+
+# value_counts依據type做統計
+# ex: 按字母順序排列結果: value_counts().sort_index(ascending=True) 
+# ex: 結果包括NA: value_counts(dropna=False)
+print(dis_institution['type'].value_counts()) 
+
+
+# 3.Rename 臺北市身障社區長照機構資料
+print("Rename 臺北市身障社區長照機構資料")
+print(dis_longterm.head())
+
+col_map = {
+    '機構類型': 'type',
+    '機構名稱': 'name',
+    '地址': 'addr',
+    '電話': 'tel'
+}
+dis_longterm.rename(columns=col_map, inplace=True) #inplace=True修改原資料
+
+print("Rename 臺北市身障設施資料")
+dis_institution.rename(columns={'lon': 'lng'}, inplace=True)
+
+# 4.Define data type
+print("Define data type (請參考Example1)")
+
+
+# 5.Fill missing value
+print("Fill missing value (這部份範例沒有實作，單純看我們自己要怎麼處裡)")
+
+
+
+# 6....... other process ......
+print("其他處理")
+print("臺北市身障社區長照機構須加上屬性")
+dis_longterm['attr'] = '待查'
+
+print("臺北市身障社區長照機構須加上行政區")
+# 正則表達式取得指定字串，'臺北市([非數字的]{2個字}區)'
+# https://docs.python.org/zh-tw/3/howto/regex.html
+dis_longterm['town'] = dis_longterm['addr'].str.extract('臺北市([^\\d]{2}區)')
+
+print(dis_longterm)
+
+
+
+"""
+print("常用資料處理function")
+
+# read KML (讀取KML圖資檔案，Google Earth支援)
+gpd.io.file.fiona.drvsupport.supported_drivers['KML'] = 'rw'
+df = gpd.read_file(files, driver='KML')
+
+# Cleaning
+data[col].fillna(0) #用 0 取代 nan
+.fillna(value=0, inplace=True) # 用 0 取代 nan
+.fillna(value=df['Col'].mean(), inplace=True) #用平均值取代 nan
+.zfill(宽度) #依據宽度用零填充字符串的左侧
+
+# Reshape
+
+disability_welfare.melt()
+# 當我們需要多層級分拆資料時，透過 melt 功能就能省去繁雜的操作
+# .melt(frame, id_vars=None, value_vars=None, var_name=None, value_name='value', col_level=None)
+# frame : 你想要更動的 DataFrame。
+# id_vars: 可使用 tuple、list、或 ndarray，用以設定不想要被轉換的欄位。
+# value_vars: 可使用 tuple、list、或 ndarray，用以設定想要被拆解的欄位。 如果省略則拆解全部欄位。
+# var_name : 轉換後 id 的名稱。如果省略則設定為原本 DataFrame 的欄位名稱或是 variable。
+# value_name : 轉換後 value 欄位的名稱。如果省略則顯示原本 DataFrame 的欄位名稱或 value。
+# col_level : 可使用 int、string。如果 columns 是 MultiIndex，則使用該參數來進行選擇。
+# ex: pd.melt(df, id_var(['B'], value_var(['A']), var_name='varName', value_name='valName')
+
+
+disability_welfare.pivot()
+# 重塑DataFrame
+# DataFrame.pivot(*, columns, index=_NoDefault.no_default, values=_NoDefault.no_default)
+# ex: df.pivot(index='foo', columns='bar', values='baz')
+# https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.pivot.html
+
+set(dis_longterm['id']) - set(dis_institution['id'])
+left_outer = dis_longterm.merge(dis_institution, how='left', on='id')
+left_outer.loc[left_outer[]'id_y'].isna()]
+
+# Transformation
+select_col = ['年份', '日間及住宿式照顧補助金額(元)', '生活補助金額(元)', '輔具補助金額(元)']
+disability_welfare = disability_welfare[select_col]
+cut, qcut
+time
+
+# Aggregation
+groupby(['col1', 'col2']).agg({'': 'nunique'})
+"""
+
+
+"""
+# 空間資料處理 (geopandas)
+
+GeoPandas implements two main data structures, a GeoSeries and a GeoDataFrame. These are subclasses of pandas.Series and pandas.DataFrame, respectively.
+
+GeoPandas has three basic classes of geometric objects (which are actually shapely objects):
+- Points / Multi-Points
+- Lines / Multi-Lines
+- Polygons / Multi-Polygons
+
+Note that all entries in a GeoSeries need not be of the same geometric type, although certain export operations will fail if this is not the case.
+"""
+
+print("空間資料處理 (geopandas)")
+addr_0 = dis_longterm.loc[0, 'addr'] #獲取地址欄位的第一筆資料
+addr_0_info = geocoder.arcgis(addr_0) #查詢座標資訊
+print(addr_0_info.json)
+# 從查詢結果取出經緯度
+addr_0_lng = addr_0_info.json['lng']
+addr_0_lat = addr_0_info.json['lat']
+
+
+# 第二筆反查，取出經緯度
+addr_1 = dis_longterm.loc[1, 'addr'] #獲取地址欄位的第二筆資料
+addr_1_info = geocoder.arcgis(addr_1)
+# 從查詢結果取出經緯度
+addr_1_lng = addr_1_info.json['lng']
+addr_1_lat = addr_1_info.json['lat']
+
+# transfer string to Point
+point0 = Point(addr_0_lng, addr_0_lat) #製作座標點0
+point1 = Point(addr_1_lng, addr_1_lat) #製作座標點1
+
+# get info of point0
+print(point0.area) #製作座標點面積
+print(point0.bounds) # minx, miny, maxx, maxy #製作座標點面積範圍
+print(point0.geom_type) #座標類型 (Points、Lines、Polygons)
+print(point0.distance(point1)) #製作座標點0與1距離
+
+"""
+Print("GeoPanda具體用法")
+
+# point
+print(Point(3, 4))
+
+# line
+print(LineString([(1, 2), (3, 4)]))
+
+# polygon
+print(Polygon([(1, 2), (3, 4), (5, 6)]))
+
+# GeoSeries
+s = gpd.GeoSeries(
+    [
+        Polygon([(0, 0), (0, 1), (1, 1), (1, 0)]),
+        Polygon([(10, 0), (10, 5), (0, 0)]),
+        Polygon([(0, 0), (2, 2), (2, 0)]),
+        LineString([(0, 0), (1, 1), (0, 1)]),
+        Point(0, 1)
+    ]
+)
+
+# GeoDataFrame
+d = {
+    'col1': ['name1', 'name2'],
+    'geometry': [Point(1, 2), Point(2, 1)]
+}
+gdf = gpd.GeoDataFrame(d, crs='EPSG:4326', geometry='geometry')
+
+# buffer
+# 緩衝區，緩衝區用於表示點、線、面等向量資料的影響範圍或服務範圍
+# buffer (distance, resolution)
+# distance：用於指定向外緩衝的距離
+# resolution：Polygon類型總是由有限個點所構成的，因此需要近似拼接出圓形的輪廓，resolution參數就用來決定每個四分之一圓弧上使用多少段連續的線段來近似拼接以表示圓的形狀，預設參數值為16，足以近似模擬圓面積的99.8%
+# https://www.cnblogs.com/feffery/p/12909284.html
+point0.buffer(1) #給予半徑為1m的緩衝區
+point0.buffer(1, resolution=1) # 
+
+
+# crs (Coordinate Reference System)
+# 'EPSG:3826' : Taiwan, bounds: (119.99, 20.41, 122.06, 26.72)
+# - X[east]: Easting (metre)
+# - Y[north]: Northing (metre)
+
+# 'EPSG:4326' : World, bounds: (-180.0, -90.0, 180.0, 90.0)
+# - Lat[north]: Geodetic latitude (degree)
+# - Lon[east]: Geodetic longitude (degree)
+
+"""
+# get 臺北市區界圖 from data.taipei
+URL = 'https://data.taipei/api/frontstage/tpeod/dataset/resource.download?rid=d8b7eb29-136f-49fc-b14b-3489d3656122'
+FILE_NAME = '.\Datasets\Raw\臺北市區界圖_20220915'
+district_border = gpd.read_file(FILE_NAME, encoding='utf-8')
+district_border.head(2)
+
+# # change crs
+# district_border = district_border.to_crs('EPSG:3826') #轉台灣常用座標格式
+
+# # get buffer
+# dis_longterm['buffer'] = dis_longterm['geometry'].buffer(1000) #回傳給定距離內所有點的近似表示。
+# print(dis_longterm.head(3))
+# buffer0 = dis_longterm.loc[0, 'buffer'] #獲取buffer第一筆
+
+
+
+print("本範例缺點: ")
+
+
+
+
